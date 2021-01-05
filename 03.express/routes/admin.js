4d57d29fdf9809c8d2484d811f17e65964d175b3
tldms <tldms201@naver.com>
2021-01-05 23:43:21 +0900

    Routing - adding admin, admin/products url

--- 03.express/routes/admin.js
+++ 03.express/routes/admin.js
@@ -4,5 +4,7 @@ const router = express.Router(); //express로 Router을 가져옴
 router.get('/', (req,res) => {
     res.send('admin app');
 });
-
+router.get('/products', (req,res) => {
+    res.send('admin products');
+});
 module.exports = router; 
