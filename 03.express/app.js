6b7094c501848c1e2d95218876f4656151009538
tldms <tldms201@naver.com>
2021-01-05 23:58:30 +0900

    View Engine - Nunjucks

--- 03.express/app.js
+++ 03.express/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const admin = require('./routes/admin'); //admin 불러옴
 
 const app = express();
 const port = 3000;
+
+
+nunjucks.configure('template', {
+    autoescape: true,
+    express: app // app 객체 선택
+});
+
 
 app.get('/', (req,res) => {
     res.send('express start');
--- 03.express/routes/admin.js
+++ 03.express/routes/admin.js
@@ -4,7 +4,11 @@ const router = express.Router(); //express로 Router을 가져옴
 router.get('/', (req,res) => {
     res.send('admin app');
 });
-router.get('/products', (req,res) => {
-    res.send('admin products');
+
+router.get('/products', function( _ , res){
+    res.render( 'admin/products.html' , 
+        { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
+    );
 });
+
 module.exports = router; 
