eb22c7a917b096d112357bd06a962aed7f627606
tldms <tldms201@naver.com>
2021-01-10 23:55:28 +0900

    DB connection

--- 04.sequelize/app.js
+++ 04.sequelize/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
-
+// db 관련
+const db = require('./models');
 class App {
 
     constructor () {
         this.app = express();
+        
+        // db 접속
+        this.dbConnection();
         
         // 뷰엔진 셋팅
         this.setViewEngine();
@@ -32,7 +35,20 @@ class App {
 
 
     }
-
+    
+    dbConnection(){
+        // DB authentication
+        db.sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .then(() => {
+            console.log('DB Sync complete.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+    }
 
     setMiddleWare (){
         
