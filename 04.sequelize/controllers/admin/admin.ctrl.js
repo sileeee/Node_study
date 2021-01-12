65e05417efe6fbbe55545d61a88406d5bea475a0
tldms <tldms201@naver.com>
2021-01-12 23:42:51 +0900

    DB - Update

--- 04.sequelize/controllers/admin/admin.ctrl.js
+++ 04.sequelize/controllers/admin/admin.ctrl.js
@@ -13,15 +13,6 @@ exports.get_products_write = ( _ , res) => {
     res.render( 'admin/write.html');
 }
 
-// exports.post_products_write = ( req , res ) => {
-//     models.Products.create({
-//         name : req.body.name,
-//         price : req.body.price ,
-//         description : req.body.description
-//     }).then( () => {
-//         res.redirect('/admin/products'); //저장하고나서 메인페이지로 이동
-//     });
-// }
 exports.post_products_write = ( req , res ) => {
     models.Products.create(req.body).then( () =>{
         models.Products.create(req.body).then( ()=>{
@@ -35,3 +26,26 @@ exports.get_products_detail = ( req , res ) => {
         res.render('admin/detail.html', { product: product });  
     });
 }; 
+
+exports.get_products_edit = ( req , res ) => {
+    models.Products.findByPk(req.params.id).then( (product) => {
+        res.render('admin/write.html', { product : product });
+    });
+};
+
+exports.post_products_edit = ( req , res ) => {
+
+    models.Products.update(
+        {
+            name : req.body.name,
+            price : req.body.price ,
+            description : req.body.description
+        }, 
+        { 
+            where : { id: req.params.id } 
+        }
+    ).then( () => {
+        res.redirect('/admin/products/detail/' + req.params.id );
+    });
+
+} 
