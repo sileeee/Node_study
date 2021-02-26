7b97f75d98fd3715a2d6136e503a6d10d0d4fd0e
tldms <tldms201@naver.com>
2021-02-26 20:39:52 +0900

    adding example of promise

--- 04.sequelize/promise_ex/make_promise.js
+++ 04.sequelize/promise_ex/make_promise.js
@@ -1,14 +1,32 @@
-const wait1seconds = new Promise(
+const aa = new Promise(
     (resolve, reject) => {
-        console.log("시작한다")
-        //1초 뒤에 콘솔에 찍습니다.
-        setTimeout(
-            () => {
-                resolve(console.log("1초뒤에 찍는다"))
-            },1000 );
-    }
-);
+        resolve(console.log("프라미스 이행"));
+    });
 
-wait1seconds.then(() =>{
-    console.log("찍고난후");
+aa.then(() =>{
+    console.log("프라미스 실행 완료");
 });
+
+// const aa = new Promise(
+//     (resolve, reject) => {
+//         resolve("프라미스123");
+//     });
+
+// aa.then((result) =>{
+//     console.log(result);
+// }); //프라미스123
+
+// const wait1seconds = new Promise(
+//     (resolve, reject) => {
+//         console.log("시작한다")
+//         //1초 뒤에 콘솔에 찍습니다.
+//         setTimeout(
+//             () => {
+//                 resolve(console.log("1초뒤에 찍는다"))
+//             },1000 );
+//     }
+// );
+
+// wait1seconds.then(() =>{
+//     console.log("찍고난후");
+// });
