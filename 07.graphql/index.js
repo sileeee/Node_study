0c47a6a669cb9de1ce6f6dcddb672bdd2de236d5
tldms <tldms201@naver.com>
2021-03-06 21:36:20 +0900

    adding data

--- 07.graphql/index.js
+++ 07.graphql/index.js
@@ -4,6 +4,12 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+    input ProductInput {
+        name: String
+        price: Int
+        description : String
+    }
+
     type Product {
         id: ID! 
         name: String
@@ -14,6 +20,11 @@ const schema = buildSchema(`
     type Query {
         getProduct ( id: ID! ): Product
     }
+
+    type Mutation {
+        addProduct(input: ProductInput): Product
+    }
+
 `); // !는 필수
 
 // 임시 제품 데이터
@@ -29,9 +40,14 @@ const products = [{
     description : "호호호"
 }]
 
-//쿼리 작성 (인자로 id를 받으면 해당 id에 일치하는 product한줄을 가져옴)
+//쿼리 작성 
 const root = { 
     getProduct : ({id}) => products.find( product => product.id === parseInt(id) ) ,
+    addProduct :  ({input}) => {
+        input.id = parseInt(products.length+1);
+        products.push(input);
+        return root.getProduct({id : input.id});
+    },
 };
 
 const app = express();
@@ -44,10 +60,3 @@ app.use('/graphql', graphqlHTTP({
 app.listen(4000, () => {
   console.log('Running a GraphQL API server at localhost:4000/graphql');
 });  
-
-// GraphiQL이나 Postman에서 
-// { getProduct(id : 1){
-//     name
-//     price
-// }}
-// 입력하여 해당 제품의 정보 출력받을 수 있음
